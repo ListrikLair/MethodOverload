8e6bbab73260a044c0dc3b963fcc429ec59d8896
ListrikLair <kevin.strom.trondsetas@gmail.com>
2023-11-17 12:28:24 +0100

    Add project files.

---
+++ MethodOverload/OverloadTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverload
+{
+    public class OverloadTest
+    {
+        public void PrintMessage(string text, int number)
+        {
+            Console.WriteLine($"Your Text: {text} and your number: {number}");
+        }
+
+        public void PrintMessage(string text1, string text2)
+        {
+            Console.WriteLine($"Your Text: {text1} and your other text: {text2}");
+        }
+    }
+}
---
+++ MethodOverload/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace MethodOverload
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var Overload = new OverloadTest();
+            Overload.PrintMessage("Hei på deg!", 200);
+            Overload.PrintMessage("Hvordan går det?", "Bare bra takk!");
+        }
+    }
+}
